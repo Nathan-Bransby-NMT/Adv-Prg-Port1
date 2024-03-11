2f792f8f4a1aa45b996a122ef74ca32b15073da4
Nathan Bransby <141193650+Nathan-Bransby-NMT@users.noreply.github.com>
2024-03-12 02:34:58 +0800

    Feat: Create unit tests for password hashing methods.

--- test/player_test.py
+++ test/player_test.py
@@ -4,16 +4,30 @@ from app import Player
 
 
 class PlayerUnitTests(unittest.TestCase):
-    def setUp(self) -> None:
-        self.test_uid = '007'
-        self.test_name = '<NAME>'
-        self.player = Player(self.test_uid, self.test_name)
+    
+    test_uid: str = '007'
+    test_name: str = '<NAME>'
+    test_password: str = '<PASSWORD>'
+
+    def setUp(self) -> None:    
+        self.player = Player(self.test_uid, self.test_name, self.test_password)
 
     def test_uid(self):
         self.assertEqual(self.player.uid, self.test_uid, "Player Unique ID's do not match.")
 
     def test_name(self):
         self.assertEqual(self.player.name, self.test_name, "Player Name does not match.")
+    
+    def test_password_is_hashed_uniquely(self):
+        other_hasher, other_hashed_password = Player.add_password(self.test_password) 
+        self.assertFalse(self.player._password == other_hashed_password, "The password was not unique.")
+        self.assertFalse(self.player._hasher == other_hasher, "The hasher object was not unique.")
+        
+    def test_password_hashes(self):
+        self.assertFalse(self.test_password == self.player._password.decode(), "The password didnt hash.")
+    
+    def test_password_is_bytes(self):
+        self.assertTrue(isinstance(self.player._password, bytes), "The password was not stored as bytes.")
 
 
 if __name__ == '__main__':
